6fbb07450f7947d8f7a8db7d396423665871943c
RachidoRoyaume <149799041+RachidoRoyaume@users.noreply.github.com>
2023-11-03 14:57:04 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    app: {
+        px: '-',
+        token: 'MTEzODU1OTg5OTUzNDYyNjgyNg.GJxSvk.tCN7LY3TrfoKwXinD3GZ8ZjIkH6uRuIE0pd6j4',
+        owners: '774361517042171924',
+        funny: '774361517042171924',
+        color: '#00001',
+        footer: 'Royaume Abus',
+        maxserver: '3',
+        everyoneMention: false,
+        hostedBy: true,
+        }
+    }
